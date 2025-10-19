5ac05ac33fca85e25c8a5dba3d41428b6d181439
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-19 13:42:18 +0200

    build: adds instances of the class and checks if the difrents objects can be reegistered inside the class in doferennt orders and types

--- Exercise1/src/main/java/Main.java
+++ Exercise1/src/main/java/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        NoGenericMethods test = new NoGenericMethods("hello", 0, 9.8 );
+        NoGenericMethods test2 = new NoGenericMethods(9, "adios", 'C' );
+
+        System.out.println(test.getObject1().getClass());
+        System.out.println(test.getObject2().getClass());
+        System.out.println(test.getObject3().getClass());
+
+        System.out.println(test2.getObject1().getClass());
+        System.out.println(test2.getObject2().getClass());
+        System.out.println(test2.getObject3().getClass());
+
+
+
     }
 }
