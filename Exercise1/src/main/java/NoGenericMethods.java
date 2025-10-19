b3bf903cf4ca887163f1840799c58cdbfc43a7e9
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-19 13:28:34 +0200

    build: creates NoGenericMethods class with constructor and fields

---
+++ Exercise1/src/main/java/NoGenericMethods.java
@@ -0,0 +1,14 @@
+public class NoGenericMethods {
+
+    private Object objeto1;
+    private Object objeto2;
+    private Object objeto3;
+
+
+    public NoGenericMethods(Object objA, Object objB, Object objC) {
+        this.objeto1 = objA;
+        this.objeto2 = objB;
+        this.objeto3 = objC;
+    }
+
+}
