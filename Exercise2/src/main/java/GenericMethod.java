d15e2bc2692abd3fd0a566c5c62a6ebc98eeccfd
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-19 14:14:24 +0200

    build: cosntruct generic method

--- Exercise2/src/main/java/GenericMethod.java
+++ Exercise2/src/main/java/GenericMethod.java
@@ -1,2 +1,10 @@
 public class GenericMethod {
+
+    public <T, U, X>  void printInfo(U arg1, T arg2, X arg3){
+        System.out.println(arg1.getClass().getName());
+        System.out.println(arg2.getClass().getName());
+        System.out.println(arg3.getClass().getName());
+
+
+    }
 }
