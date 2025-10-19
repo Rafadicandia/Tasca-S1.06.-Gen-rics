a8390fc4c2e718e59c0cb3d4fdc79018a313a477
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-19 14:49:47 +0200

    test: tests methods in main

--- Exercise2/src/main/java/GenericMethod.java
+++ Exercise2/src/main/java/GenericMethod.java
@@ -1,6 +1,6 @@
 public class GenericMethod {
 
-    public <T, U, X>  void printInfo(U arg1, T arg2, X arg3){
+    public static <T, U, X>  void printInfo(U arg1, T arg2, X arg3){
         System.out.println(arg1.getClass().getName());
         System.out.println(arg2.getClass().getName());
         System.out.println(arg3.getClass().getName());
---
+++ Exercise2/src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Pepe", "Pepino", 45);
+
+        GenericMethod.printInfo(person, "Hello", 6);
+
+
+
+
+
+
+    }
+}
