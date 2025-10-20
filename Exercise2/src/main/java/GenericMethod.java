ad80275026e15fc0c597b5d2de8487c469ce3a08
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-20 12:48:20 +0200

    refactor

--- Exercise2/src/main/java/GenericMethod.java
+++ Exercise2/src/main/java/GenericMethod.java
@@ -3,7 +3,7 @@ public class GenericMethod {
     public static <T, U, X>  void printInfo(U arg1, T arg2, X arg3){
         System.out.println("First parameter: " +arg1);
         System.out.println("Second parameter: " +arg2);
-        System.out.println("Second parameter: " +arg3);
+        System.out.println("Third parameter: " +arg3);
 
 
     }
